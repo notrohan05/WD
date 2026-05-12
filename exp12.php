8c845eda6d1b80927716ea055e8ba8c56d85bed9
notrohan05 <rohankadam0125@gmail.com>
2026-05-13 00:39:53 +0530

    Update exp12.php

--- exp12.php
+++ exp12.php
@@ -49,3 +49,79 @@ Arrays are useful when we need to store and manage a collection of similar data 
 
 Conclusion-
 Thus, we successfully created a PHP program to store and display array elements using the foreach loop. This program helped in understanding the concept of arrays and iteration in PHP.
+
+VIVA QUESTIONS:
+
+1. What is the purpose of this program?
+This program stores multiple subjects in an array and displays them using a loop.
+
+2. What is PHP?
+PHP stands for Hypertext Preprocessor. It is a server-side scripting language.
+
+3. What is an array?
+An array is a variable that stores multiple values in a single variable.
+
+4. Which type of array is used in this program?
+An indexed array is used.
+Example:
+$subjects = ["DBMS", "PHP", "Java", "Python", "OS"];
+
+5. What are indexed arrays?
+Indexed arrays store values with numeric indexes starting from 0.
+
+6. What is stored in $subjects?
+The $subjects array stores subject names:
+DBMS
+PHP
+Java
+Python
+OS
+
+7. What is the use of foreach loop?
+The foreach loop is used to access array elements one by one.
+Example:
+foreach($subjects as $sub)
+
+8. What does $sub represent?
+$sub stores one array element at a time during each loop iteration.
+
+9. How many times will the loop execute?
+The loop executes 5 times because there are 5 subjects.
+
+10. What is the output of this program?
+The program displays all subjects line by line.
+
+11. Why is <br> used in echo statement?
+<br> creates a new line in HTML output.
+
+12. What is the use of echo in PHP?
+echo displays output on the webpage.
+
+13. Can arrays store different data types?
+Yes, arrays can store strings, numbers, and other data types together.
+
+14. What is the index number of "Java"?
+The index of "Java" is 2.
+
+15. What is the first element of the array?
+DBMS
+
+16. What is the last element of the array?
+OS
+
+17. What is the difference between for loop and foreach loop?
+for loop uses indexes.
+foreach directly accesses array elements.
+
+18. Which extension is used for PHP files?
+.php
+Example:
+array.php
+
+19. Which software is used to run PHP programs?
+Examples:
+XAMPP
+WAMP
+
+20. What is server-side scripting?
+Server-side scripting means code executes on the server before showing output in browser.
